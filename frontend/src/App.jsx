e61642754bfe2f919adc01304d7f682e091d9cda
Balda Naresh <nareshbalda0@gmail.com>
2024-07-22 21:02:46 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+//import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <h1 className='text-4xl text-red-600'>hello world</h1>
+    </>
+  )
+}
+
+export default App
